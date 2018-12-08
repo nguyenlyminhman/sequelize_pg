70f2abb30bfb12d964ffafd73129104d3e86bed1
Nguyen Ly Minh Man <nguyenlyminhman@gmail.com>
2018-12-08 23:24:46 +0700

    add body-parser

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const bodyParser = require('body-parser')
 
 const models = require('./model/configDb')
 const router = require('./router/index')
+const parser = require('body-parser').urlencoded({ extended: true });
 
 const app = express()
-app.use(bodyParser.json())
 
+app.use(parser)
 
 app.use('/api', router)
 
 app.listen(3000, ()=>{
-    console.log('Server is running on host 3000')
+    console.log('Server is running on host', 3000)
 })
--- router/index.js
+++ router/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const { User, Blog, Tag } = require('../model/configDb')
 
 router.get('/user', (req, res) => {
@@ -8,8 +7,9 @@ router.get('/user', (req, res) => {
 });
 
 router.post('/user', (req,res)=>{
-    const {name} = req.body
-    User.create({name:name}).then(user => res.json(user)).catch(err=>console.log(err))
+    let {name} = req.body
+    console.log(name)
+    User.create({name:name}).then(user => res.send(user)).catch(err=>console.log(err))
 })
 
 
